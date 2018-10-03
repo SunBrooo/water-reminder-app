2d432d750d0b2e26b9c99a09d430e3468f346974
Boris K <b.kydrevich@gmail.com>
2018-10-03 13:44:42 +0300

    Initial Commit

---
+++ water reminder app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  water reminder app
+//
+//  Created by mac on 03.10.2018.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
